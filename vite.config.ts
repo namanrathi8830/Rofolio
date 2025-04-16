e8076a9ac1ecd99b5e2e5e36c9d1826ec2e30c55
namanrathi8830 <namanrathi8830@gmail.com>
2025-04-16 21:08:06 +0530

    Add Netlify configuration and fix routing issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [
     react(),
     tempo(),
